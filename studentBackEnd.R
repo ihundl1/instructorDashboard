51ed1d53e28c212ab1052b34a5c46a64b0e91482
Isabella <ihundl1@lsu.edu>
2018-11-23 19:20:42 -0600

    Value Boxes! (attendance)

--- studentBackEnd.R
+++ studentBackEnd.R
@@ -10,16 +10,22 @@ attendance <- tbl(conDatasource, 'attendance')
 classes <- tbl(conDatasource, 'attevent')
 section <- tbl(conDatasource, 'section')
 
-big <- left_join(roster, section, by=c('section'='sectionId')) %>% left_join(attendance, by='pawsId') %>% 
-  left_join(classes, by=c('att_event' = 'eventId'))
-
-big <- big %>% select(pawsId:section, instructor, delivery, att_event:eventTopic)
-
-students <- distinct(roster, pawsId) %>% collect()
-
-nameTable <- distinct(roster, pawsId, lastname, firstname) %>% collect()
+nameTable <- left_join(roster, section, by = c('section' = 'sectionId')) %>% 
+  filter(delivery == "inclass") %>% distinct(pawsId, lastname, firstname) %>% arrange(lastname) %>% 
+  collect()
 nameList <- c()
 for (row in 1:nrow(nameTable)) {
   index <- paste0(nameTable[row, 'lastname'], ', ', nameTable[row, 'firstname'])
   nameList[index] <- as.character(nameTable[row, "pawsId"])
 }
+
+course <- classes %>% collect() %>% mutate(sectionId = substr(eventId, 1, 7)) %>%
+  group_by(sectionId, eventTopic) %>% summarise(classTotal = n())
+
+attend <- attendance %>% left_join(classes, by = c('att_event' = 'eventId')) %>% collect() %>%
+  group_by(pawsId, eventTopic) %>% summarise(att = n())
+
+big <- section %>% filter(delivery == "inclass") %>% select(sectionId, instructor) %>% 
+  left_join(roster, by = c('sectionId' = 'section')) %>% collect() %>% 
+  left_join(attend, by = 'pawsId') %>% left_join(course, by = c('sectionId', 'eventTopic')) %>%
+  mutate(attPerc = att / classTotal) %>% mutate(missed = classTotal - att)
